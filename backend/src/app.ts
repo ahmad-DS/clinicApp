4b4789aa06313ff34db44e875a2ecd262afb3f12
ahmad-DS <ahmad.shakil444@gmail.com>
2026-06-12 21:12:05 +0530

    adds frontend deployed url to backend to allow cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // app.use(cors());
 app.use(cors({
-  origin: 'https://your-frontend-app.vercel.app',
+  origin: 'https://warshihospitals.netlify.app',
   credentials: true
 }));
 
