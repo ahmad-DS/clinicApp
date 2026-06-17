4ec9555d7689f6154e8a1c7a7d399c61d5dae26f
ahmad-DS <ahmad.shakil444@gmail.com>
2026-06-17 16:29:08 +0530

    adds search functionality in patient directory page

--- frontend/src/pages/patients/PatientDirectory.tsx
+++ frontend/src/pages/patients/PatientDirectory.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import type { RootState, AppDispatch } from '../../store';
 import { fetchPatients, createBackendPatient } from '../../store/medicalSlice';
